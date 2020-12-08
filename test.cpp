955c878fc0515959791fac7d910542497d370903
HaoyuWang <751984964@qq.com>
2020-12-08 16:44:32 +0800

    读取shp

--- test.cpp
+++ test.cpp
@@ -1,25 +1,57 @@
-#include<iostream>  
-#include"gdal.h"  
-#include"gdal_priv.h"  
+#include "gdal.h"
 
-using namespace std;  
+#include "ogr_api.h"
+#include "ogrsf_frmts.h"
 
-int main(){  
+#include <stdio.h>
+#include <iostream>
 
-	cout << "hello world" << endl;  
+using namespace std;
 
-	char* fileName = "D:\\LC81200382013335LGN00_BQA.TIF";  
-	GDALDataset *poDataset;     //GDAL数据集  
-	GDALAllRegister();  
+int main()
+{
+	//注册所有文件格式驱动
+	GDALAllRegister();
+	OGRRegisterAll();
 
-	poDataset = (GDALDataset*)GDALOpen(fileName, GA_ReadOnly);  
+	 //解决中文乱码问题
+	CPLSetConfigOption("SHAPE_ENCODING",""); 
 
-	if (poDataset == NULL){  
-		cout << "文件打开错误" << endl;  
-	}  
-	else{  
-		cout << "文件打开成功" << endl;  
-	}  
+	//得到对应文件类型处理器
+	OGRSFDriver* poDriver = OGRSFDriverRegistrar::GetRegistrar()->GetDriverByName("ESRI Shapefile");
 
-	system("pause");  
-}  
+	//打开文件
+	OGRDataSource* poDS = poDriver->Open( "D:\\bou2_4p.shp", NULL );
+
+	if( poDS == NULL )
+	{
+		printf( "Open failed.\n%s" );
+		return 0;
+	}
+	OGRLayer  *poLayer;
+	poLayer = poDS->GetLayer(0); //读取层
+	OGRFeature *poFeature;
+
+	poLayer->ResetReading();
+	int i=0;
+	while( (poFeature = poLayer->GetNextFeature()) != NULL )
+	{
+		if(poFeature->GetFieldAsDouble("AREA")<1) continue; //去掉面积过小的polygon
+		i=i++;
+		cout<<i<<"  ";
+		OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
+		int iField;
+		int n=poFDefn->GetFieldCount(); //获得字段的数目，不包括前两个字段（FID,Shape);
+		for( iField = 0; iField <n; iField++ )
+		{
+			//输出每个字段的值
+			cout<<poFeature->GetFieldAsString(iField)<<"    ";
+		}
+		cout<<endl;
+		OGRFeature::DestroyFeature( poFeature );
+	}
+	GDALClose( poDS );
+	system("pause");
+
+	return 0;
+}
